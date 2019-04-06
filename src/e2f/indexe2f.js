8a971521f3c337872b16d4e14facc7f924f4f214
himadri-repo <himadri.trainer@gmail.com>
2019-04-07 00:33:35 +0530

    e2f code changes

--- src/e2f/indexe2f.js
+++ src/e2f/indexe2f.js
@@ -10,33 +10,63 @@ const uuidv4 = require('uuid/v4');
 const moment = require('moment');
 
 import {Logger, E2FCrawler} from './e2fcrwler';
-import { loggers } from 'winston';
+//import { loggers } from 'winston';
 
-Logger.log('info', "Starting process ...");
+let app = express();
 
-let e2fcrawler = new E2FCrawler({url: '', output: 'json'});
-let response = e2fcrawler.postData({url: 'https://expressdev.ease2fly.com/api/destinations/get-destinations-list', data: {
-    usrId: 109,
-    usrType: "N"
-}});
+function startProcess() {
+    Logger.log('info', "Starting process ...");
 
-response.then(data => {
-    Logger.log("info", JSON.stringify(data.result));
+    let e2fcrawler = new E2FCrawler({url: '', output: 'json'});
+    let response = e2fcrawler.postData({url: 'https://expressdev.ease2fly.com/api/destinations/get-destinations-list', data: {
+        usrId: 109,
+        usrType: "N"
+    }});
 
-    Logger.log('info', JSON.stringify(e2fcrawler.finalData));
+    response.then(data => {
+        Logger.log("info", JSON.stringify(data.result));
 
-    let runid = `${uuidv4()}_${moment().format("DD-MMM-YYYY HH:mm:ss.SSS")}`;
-    datstore.saveCircleBatchData(runid, e2fcrawler.finalData, "", function(rrid) {
-        datstore.finalization(rrid, () => {
-            Logger.log('info', 'Finished');
+        Logger.log('info', JSON.stringify(e2fcrawler.finalData));
 
-            process.removeAllListeners("unhandledRejection");
-            process.removeAllListeners('exit');
-            process.removeAllListeners();
+        let runid = `${uuidv4()}_${moment().format("DD-MMM-YYYY HH:mm:ss.SSS")}`;
+        datstore.saveCircleBatchData(runid, e2fcrawler.finalData, "", function(rrid) {
+            datstore.finalization(rrid, () => {
+                Logger.log('info', 'Finished');
+
+                process.removeAllListeners("unhandledRejection");
+                process.removeAllListeners('exit');
+                process.removeAllListeners();
+                return;
+            });
             return;
         });
-        return;
+    }).catch(error => {
+        Logger.log("error", error);
     });
-}).catch(error => {
-    Logger.log("error", error);
+}
+
+var excutionStarted = false;
+cron.schedule("*/10 * * * *", function() {
+    Logger.log("info", "Cron started");
+    if(excutionStarted) {
+        Logger.log("info", 'Previous process still running ...');
+        return false;
+    }
+
+    try
+    {
+        excutionStarted = true;
+        process.on('unhandledRejection', (reason, promise) => {
+            Logger.log('info','Unhandled Rejection at:', reason);
+        });
+        startProcess();
+    }
+    catch(e) {
+        Logger.log('error', e);
+    }
+    finally {
+        excutionStarted = false;
+    }
 });
+
+app.listen("3232");
